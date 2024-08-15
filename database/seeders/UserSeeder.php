cea9f505aee992a600741b0b6d0916b05964081e
MaxFelis <m.felis@flooris.nl>
2024-08-15 08:25:48 +0200

    Added basic user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::updateOrCreate(
+            [
+                'name' => 'Max',
+            ],
+            [
+                'email'             => 'contact@felis-ts.nl',
+                'password'          => bcrypt(config('auth.user_password')),
+                'email_verified_at' => now(),
+            ]
+        );
+    }
+}
