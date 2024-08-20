f880821ea07f8d4e6607ac55e846a0029730008c
MaxFelis <m.felis@flooris.nl>
2024-08-20 19:01:41 +0200

    Event invite

--- resources/js/Pages/Events/EventInvite.vue
+++ resources/js/Pages/Events/EventInvite.vue
@@ -6,24 +6,40 @@ defineProps({
         type: Object,
         required: true
     }
-})
+});
 </script>
 
 <template>
-  <DefaultLayout>
-      <div class="py-16 md:py-24 px-6 flex flex-col items-center justify-center bg-slate-100 rounded">
-          <div class="w-full flex justify-center text-2xl font-semibold">
-              <h1>
-                  {{ event.title}}
-              </h1>
-          </div>
-
-          <div class="flex flex-col items-center w-full ">
-              {{ event.description }}
-          </div>
-          <div class="flex flex-col items-center w-full ">
-              {{ event.location }}
-          </div>
-      </div>
-  </DefaultLayout>
+    <DefaultLayout>
+        <div class="py-16 md:py-24 flex flex-col items-center justify-center bg-slate-100 rounded">
+            <div class="w-full lg:w-1/2 border bg-white">
+                <div class="flex flex-col justify-center w-full items-center bg-white py-16">
+                    <span class="text-xl pb-2">
+                        You are invited to:
+                    </span>
+                    <span class="text-2xl my-2">
+                        {{ event.title }}
+                    </span>
+                    <span class="w-10/12">
+                        Event description:
+                    </span>
+                    <div
+                        class="bg-slate-100 p-2 w-10/12 rounded pt-1 mb-4"
+                        :class="event.description ?? 'italic text-gray-800' "
+                    >
+                        {{ event.description ?? 'No event description available.'}}
+                    </div>
+                    <span class="w-10/12">
+                        Event location:
+                    </span>
+                    <div
+                        class="bg-slate-100 p-3 w-10/12"
+                        :class="event.location ?? 'italic text-gray-800' "
+                    >
+                        {{ event.location ?? 'No event location available.'}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </DefaultLayout>
 </template>
