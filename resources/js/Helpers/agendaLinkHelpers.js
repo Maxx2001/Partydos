c64ffb9659ffe26f55d9a955271c65e8bd42d545
MaxFelis <m.felis@flooris.nl>
2024-08-30 16:56:01 +0200

    Google calendar event test

--- resources/js/Helpers/agendaLinkHelpers.js
+++ resources/js/Helpers/agendaLinkHelpers.js
@@ -11,11 +11,11 @@ const googleCalendarLink =  (
     const checkedIsoEndDateTime = isoEndDateTime ? isoEndDateTime : isoStartDateTime;
 
     if(isMobile()) {
-        return `intent://calendar/render?action=TEMPLATE&text=${encodeURIComponent(
+        return `intent://details?action=TEMPLATE&text=${encodeURIComponent(
             title
         )}&dates=${isoStartDateTime}/${checkedIsoEndDateTime}&details=${encodeURIComponent(
             description
-        )}&location=${encodeURIComponent(location)}#Intent;scheme=https;package=com.google.android.calendar;S.browser_fallback_url=https%3A%2F%2Fcalendar.google.com;end;`;
+        )}&location=${encodeURIComponent(location)}#Intent;scheme=https;package=com.google.android.calendar;S.browser_fallback_url=https%3A%2F%2Fcalendar.google.com%2Fcalendar%2Fr%2Feventedit;end;`;
     }
 
     return `https://www.google.com/calendar/render?action=TEMPLATE&text=${encodeURIComponent(
