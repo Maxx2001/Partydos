829266c9e904962837b8b0a7b064a4d0907d988a
MaxFelis <m.felis@flooris.nl>
2024-08-30 16:59:47 +0200

    Google calendar event test

--- resources/js/Helpers/agendaLinkHelpers.js
+++ resources/js/Helpers/agendaLinkHelpers.js
@@ -1,27 +1,14 @@
-function isMobile() {
-    return /Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
-}
-
 const googleCalendarLink =  (
-    isoStartDateTime,
-    isoEndDateTime,
-    title,
-    description,
-    location) =>  {
-    const checkedIsoEndDateTime = isoEndDateTime ? isoEndDateTime : isoStartDateTime;
-
-    if(isMobile()) {
-        return `intent://details?action=TEMPLATE&text=${encodeURIComponent(
+        isoStartDateTime,
+        isoEndDateTime,
+        title,
+        description,
+        location) =>  {
+        const checkedIsoEndDateTime = isoEndDateTime ? isoEndDateTime : isoStartDateTime;
+        return `https://www.google.com/calendar/render?action=TEMPLATE&text=${encodeURIComponent(
             title
-        )}&dates=${isoStartDateTime}/${checkedIsoEndDateTime}&details=${encodeURIComponent(
-            description
-        )}&location=${encodeURIComponent(location)}#Intent;scheme=https;package=com.google.android.calendar;S.browser_fallback_url=https%3A%2F%2Fcalendar.google.com%2Fcalendar%2Fr%2Feventedit;end;`;
+        )}&dates=${isoStartDateTime}/${checkedIsoEndDateTime}&details=${encodeURIComponent(description)}&location=${encodeURIComponent(location)}`;
     }
-
-    return `https://www.google.com/calendar/render?action=TEMPLATE&text=${encodeURIComponent(
-        title
-    )}&dates=${isoStartDateTime}/${checkedIsoEndDateTime}&details=${encodeURIComponent(description)}&location=${encodeURIComponent(location)}`;
-}
 
 
 export default googleCalendarLink;
