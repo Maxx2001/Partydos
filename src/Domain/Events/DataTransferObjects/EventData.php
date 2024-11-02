05af0908dc57112fbd16691374e618ce79d6ac06
Max Felis <max.felis11@gmail.com>
2024-11-02 09:30:28 +0100

    Updated Event Code

--- src/Domain/Events/DataTransferObjects/EventData.php
+++ src/Domain/Events/DataTransferObjects/EventData.php
@@ -27,25 +27,4 @@ class EventData extends Data
         public Carbon $createdAt,
         public Carbon $updatedAt
     ) {}
-
-    public static function fromModel(Event $event): self
-    {
-        return new self(
-            id: $event->id,
-            uniqueIdentifier: $event->unique_identifier,
-            title: $event->title,
-            description: $event->description ?? '',
-            location: $event->location ?? '',
-            startDateTime: $event->start_date_time,
-            isoStartDateTime: Carbon::parse($event->start_date_time)->setTimezone('UTC')->format('Ymd\THisO'),
-            endDateTime: $event->end_date_time,
-            isoEndDateTime: !$event->end_date_time ? null : Carbon::parse($event->end_date_time)->setTimezone('UTC')->format('Ymd\THisO'),
-            status: $event->status,
-            shareLink: (new EventShareLinkService())->generateShareLink($event),
-            eventOwner: EventOwnerData::getOwner($event),
-            participants: ParticipantData::fromCollection($event->guestUsers),
-            createdAt: $event->created_at,
-            updatedAt: $event->updated_at
-        );
-    }
 }
--- src/Domain/Events/DataTransferObjects/ParticipantData.php
+++ src/Domain/Events/DataTransferObjects/ParticipantData.php
@@ -13,18 +13,4 @@ class ParticipantData
         public string $name,
         public string $email
     ) {}
-
-    public static function fromModel(GuestUser|User $participant): self
-    {
-        return new self(
-            id: $participant->id,
-            name: $participant->name,
-            email: $participant->email
-        );
-    }
-
-    public static function fromCollection(Collection $participants): array
-    {
-        return $participants->map(fn($participant) => self::fromModel($participant))->toArray();
-    }
 }
