34ac4f76279877b026e5ef504929ffff2dbdc1df
Max Felis <max.felis11@gmail.com>
2025-02-15 12:24:33 +0100

    feat: updated clarity script

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,11 +10,13 @@ import {createPinia} from 'pinia'; // Import Pinia
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
-(function(c,l,a,r,i,t,y){
-    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-})(window, document, "clarity", "script", "qa5ud3327i");
+if (import.meta.env.APP_ENV === 'production') {
+    (function(c, l, a, r, i, t, y) {
+        c[a] = c[a] || function() { (c[a].q = c[a].q || []).push(arguments) };
+        t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i;
+        y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
+    })(window, document, "clarity", "script", "qa5ud3327i");
+}
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
