33731419ee036b7b67adb88f5e0b0983c6dd39e4
Max Felis <max.felis11@gmail.com>
2025-03-08 14:29:42 +0100

    fix: login

--- src/Domain/Auth/Actions/LoginAction.php
+++ src/Domain/Auth/Actions/LoginAction.php
@@ -4,15 +4,19 @@ namespace Domain\Auth\Actions;
 
 use Domain\Auth\DataTransferObjects\LoginData;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class LoginAction
 {
-    public function execute(LoginData $loginData)
+    public function execute(LoginData $loginData): bool
     {
-        if (Auth::attempt($loginData->only('email', 'password')->toArray(), $loginData->remember)) {
-            return true;
+        if (!Auth::attempt($loginData->only('email', 'password')->toArray(), $loginData->remember)) {
+            throw ValidationException::withMessages([
+                'email' => __('These credentials do not match our records.')
+            ]);
+            
         }
 
-        return false;
+        return true;
     }
 }
